1a8e1aa8e49cb6702bd77dee29371db2d18354ee
codingdestro <codingdestro@gmail.com>
2024-12-13 23:24:52 +0530

    update - schemas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,20 +1,35 @@
 import { text, sqliteTable } from "drizzle-orm/sqlite-core";
+import { title } from "process";
 import { v4 as uuid } from "uuid";
 export const users = sqliteTable("users", {
   id: text("id").$defaultFn(() => uuid()),
   username: text("username", { length: 20 }),
-  password: text("password"),
   email: text("email"),
+  password: text("password"),
 });
 
 export const todos = sqliteTable("todos", {
-  id: text("id").$defaultFn(() => uuid()),
+  id: text("id")
+    .$defaultFn(() => uuid())
+    .primaryKey(),
   userId: text("user_id"),
   todo: text("todo"),
+  completed: text("completed").default("false").notNull(),
+  createdAt: text("created_at").$defaultFn(() => new Date().toISOString()),
+  updatedAt: text("created_at").$defaultFn(() => new Date().toISOString()),
 });
 
 export const groups = sqliteTable("groups", {
   id: text("id").$defaultFn(() => uuid()),
   userId: text("user_id"),
-  todoId: text("todo_id"),
+  name: text("name").notNull(),
+  createdAt: text("created_at").$defaultFn(() => new Date().toISOString()),
+});
+
+export const notes = sqliteTable("notes", {
+  id: text("id").$defaultFn(() => uuid()),
+  userId: text("user_id"),
+  title: text("title").notNull(),
+  note: text("note"),
+  createdAt: text("created_at").$defaultFn(() => new Date().toISOString()),
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,37 +1,9 @@
 import { Hono } from "hono";
-import { User } from "../types";
-import { loginUser, signInUser } from "../utils/signin";
-import { getJWTToken } from "../utils/authorization";
 
 const route = new Hono();
 
-route.get("/", (c) => {
-  return c.text("welcome to the basic server!");
-});
-
-route.post("/signin", async (c) => {
-  const body: User = await c.req.parseBody();
-  try {
-    await signInUser(body);
-    return c.text("user signed in!");
-  } catch (err) {
-    console.log(err);
-    return c.text("failed to register a new user");
-  }
-});
-
-route.post("/login", async (c) => {
-  const user: User = await c.req.parseBody();
-  try {
-    if (!user.email && !user.password)
-      return c.text("email of password missing!");
-    await loginUser(user);
-    const token = await getJWTToken(user);
-    return c.json({ token: token });
-  } catch (err) {
-    console.log(err);
-    return c.text("failed to login");
-  }
+route.on("get", "/test/", (c) => {
+  return c.text("Hello from hono");
 });
 
 export default route;
